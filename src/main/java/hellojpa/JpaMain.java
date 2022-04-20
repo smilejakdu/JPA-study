e34b84b63842b7cbeb470c5fbddf61870397f416
smilejakdu <ash982416@gmail.com>
2022-04-20 18:47:59 +0900

    영속성 컨테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,11 +15,17 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember.id = "+findMember.getId());
-            System.out.println("findMemeber.name = "+findMember.getName());
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+//            사실 여기까지는 아무상태도 아니다. -> 비영속
+//            이렇게 해야 영속 상태
+            System.out.println("=====BEFORE====");
+            em.persist(member);
+            System.out.println("=====AFTER=====");
 
-            tx.commit();
+            tx.commit(); // 이때 데이터베이스에 쿼리가 날라간다.
+//            왜이렇게하는거지 ???
         }catch (Exception e) {
             tx.rollback();
         } finally {
